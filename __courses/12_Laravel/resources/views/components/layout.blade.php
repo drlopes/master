402515fa7c8402cc8ab753a3cd4434c1e9ae33d1
Daniel <dnielrl007@gmail.com>
2022-10-18 09:19:04 -0300

    Laravel course

---
+++ __courses/12_Laravel/resources/views/components/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>{{ $title }} | Series Logger</title>
+    </head>
+    <body>
+        <h1>{{ $title }}</h1>
+
+        {{ $slot }}
+
+    </body>
+</html>
